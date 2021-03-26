be693ea8501992160572c5effd89c8bbb58d1394
Andreas Gunnahr <andreas.gunnahr@hotmail.com>
2021-03-26 20:31:56 +0100

    fixed tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,32 @@
-
 const defaultTheme = require("tailwindcss/defaultTheme");
 
-
-
-
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       screens: {
         desktop: "1200px",
-        noPadding: "calc(1200px - 80px)"
+        noPadding: "calc(1200px - 80px)",
       },
       fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans]
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
       },
       backgroundColor: {
-        "primary": "#F9FAFB"
+        primary: "#F9FAFB",
       },
       height: {
-        'without-navbar': 'calc(100vh - 64px)',
-      }
+        "without-navbar": "calc(100vh - 64px)",
+      },
     },
   },
   variants: {
     extend: {
-      borderWidth: ['last']
+      borderWidth: ["last"],
     },
   },
-  plugins: [
-  ]
-}
+  plugins: [],
+};
